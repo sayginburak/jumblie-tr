8b9516f1d18fed59eb2056d5c171842d572b6651
Saygin Burak Sevinc <ssevinc@avallain.com>
2024-01-06 22:33:20 +0300

    Update wordlist

--- words/wordlist.js
+++ words/wordlist.js
@@ -328,6 +328,10 @@ let wordList = [
 		words: ["çapa", "fidan", "peyzaj","kompost"],
 	},
 	{
+		theme: "sebze",
+		words: ["bamya", "pırasa", "enginar","kuşkonmaz"],
+	},
+	{
 		theme: "eski uygarlıklar",
 		words: ["maya", "aztek", "fenike","hattuşa"],
 	}
