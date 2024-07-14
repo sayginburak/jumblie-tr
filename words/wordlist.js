9f6961884ba01b8dccd73242de47bf3974c10cfe
Saygın Burak Sevinç <sayginburak17@gmail.com>
2024-07-14 22:30:56 +0300

    Update wordlist.js

--- words/wordlist.js
+++ words/wordlist.js
@@ -862,7 +862,8 @@ let wordList = [
 	},
 	{theme: "sulu",words:["boya","yemek", "sepken", "çözelti"]},
 	{theme: "ortadoğu",words:["iran","basra", "süveyş", "piramit"]},
-	{theme: "hormon",words:["insülin","dopamin", "endorfin", "adrenalin"]}
+	{theme: "hormon",words:["insülin","dopamin", "endorfin", "adrenalin"]},
+	{theme: "geniş ...",words:["aile","omuz","ekran", "zaman"]}
 	
 	
 ]
